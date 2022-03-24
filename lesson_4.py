b96069ad33754a5554901a0c47bb690c717662c9
Мордовин Владимир Сергеевич <v.mordovin@tinkoff.ru>
2022-03-24 15:58:01 +0300

    fix tasks

--- lesson_4.py
+++ lesson_4.py
@@ -8,19 +8,21 @@ list = [i for i in range(1,35)]
 
 # Функция генерации списка
 
-def get_list() -> list:
-    list = [i for i in range(8,79)]
-    return list
+def get_list(begin, end) -> list:
+    return [i for i in range(begin,end)]
 
-list = get_list()
+
 
 
 # Функция генерации списка текущего времени
 
 def get_current_time():
-    list = []
-    for i in range(1, 10):
-        t = time.localtime()
-        now_time = time.strftime("%H:%M:%S", t)
-        list.append(now_time)
-    return list
+    t = time.localtime()
+    now_time = time.strftime("%H:%M:%S", t)
+    return now_time
+
+list = []
+for i in range(1, 15):
+    times = get_current_time()
+    list.append(times)
+print(list)
