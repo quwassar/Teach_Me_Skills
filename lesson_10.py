1d8c443b37701899f2158e4cd4e6e2f3e23e2f2a
Мордовин Владимир Сергеевич <v.mordovin@tinkoff.ru>
2022-04-12 11:44:54 +0300

    fix except, add func for get data

--- lesson_10.py
+++ lesson_10.py
@@ -1,14 +1,18 @@
-class WrongSymbol(Exception):
+class WrongSymbolException(Exception):
 
-    def exception_mark(mark):
+    def search_exception_mark(mark):
         marks = ('+', '-', '*', '**', '/', '//', '%')
         if mark != [symbol for symbol in marks]:
             print('Это что за символы? Я с ними работать не буду')
 
-try:
+def get_data():
     first = float(input("Первое число: "))
     mark = input("Знак вычисления: ")
     second = float(input("Второе число: "))
+    return first, mark, second
+
+try:
+    first, mark, second = get_data()
     if mark == '+':
         print(first+second)
     elif mark == '-':
@@ -23,8 +27,7 @@ try:
         print(first//second)
     elif mark == '%':
         print(first%second)
-    else:
-        WrongSymbol.exception_mark(mark=mark)
+    raise (WrongSymbolException)
     
 except ZeroDivisionError:
     print('На ноль делить нельзя')
@@ -36,5 +39,5 @@ except TypeError as e:
 except ValueError:
     print('Что за фигня? Тут должны быть цифры и знаки вычисления')
 
-# except WrongSymbol:
-#     WrongSymbol.exception_mark(mark=mark)
+except WrongSymbolException:
+    WrongSymbolException.search_exception_mark(mark=mark)
