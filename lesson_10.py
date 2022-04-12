91b6bb6cb3540cd9a3c776857ac2ef16d894acaf
Мордовин Владимир Сергеевич <v.mordovin@tinkoff.ru>
2022-04-12 10:51:37 +0300

    add my exception

--- lesson_10.py
+++ lesson_10.py
@@ -1,4 +1,9 @@
-marks = ('+', '-', '*', '**', '/', '//', '%')
+class WrongSymbol(Exception):
+
+    def exception_mark(mark):
+        marks = ('+', '-', '*', '**', '/', '//', '%')
+        if mark != [symbol for symbol in marks]:
+            print('Это что за символы? Я с ними работать не буду')
 
 try:
     first = float(input("Первое число: "))
@@ -18,15 +23,18 @@ try:
         print(first//second)
     elif mark == '%':
         print(first%second)
+    else:
+        WrongSymbol.exception_mark(mark=mark)
     
 except ZeroDivisionError:
     print('На ноль делить нельзя')
 
-except TypeError:
-    print('Кажется, был указан лишний знак при вводе')
+except TypeError as e:
+    print(e)
+    print('Кажется, был указан не тот знак при вводе')
 
 except ValueError:
     print('Что за фигня? Тут должны быть цифры и знаки вычисления')
 
-except Exception:
-    print('Test')
+# except WrongSymbol:
+#     WrongSymbol.exception_mark(mark=mark)
