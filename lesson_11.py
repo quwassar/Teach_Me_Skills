870ca9b04499678ab5e2f4d177355c0f0fdda100
Мордовин Владимир Сергеевич <v.mordovin@tinkoff.ru>
2022-04-14 11:52:13 +0300

    fix name

--- lesson_11.py
+++ lesson_11.py
@@ -4,5 +4,5 @@ def list_nuber_generation(start_number):
     while start_number < 10000:
         start_number = start_number*start_number
         yield start_number
-list_numbers_generation = [number for number in list_nuber_generation(start_number)]
-print(list_numbers_generation)
+list_numbers_generate = [number for number in list_nuber_generation(start_number)]
+print(list_numbers_generate)
